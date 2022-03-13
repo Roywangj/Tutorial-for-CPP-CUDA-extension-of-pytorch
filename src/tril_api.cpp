d0fde51cbb93f1d433daaea3a1c66a05c285cc9a
Roywangj <48822865+Roywangj@users.noreply.github.com>
2022-03-13 13:07:57 +0800

    Delete src directory

--- src/tril_api.cpp
+++
@@ -1,7 +0,0 @@
-#include <torch/extension.h>
-#include "trillnear_devox_diff_R.h"
-
-// pybind11 绑定
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &trilinear_devoxelize_diffR_forward, "TEST forward");
-}
