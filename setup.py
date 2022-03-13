54c729929aad6c537d07b1d9b8e72f9598ac6029
Roywangj <48822865+Roywangj@users.noreply.github.com>
2022-03-13 13:06:16 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
         author_email='jwang991020@gmail.com',
         ext_modules=[
             CUDAExtension(
-            name = 'src.tri_op_cuda', 
-            sources=["src/tril_api.cpp",
-                    "src/trillnear_devox_diff_R.cpp",
-                    "src/trillnear_devox_diff_R_cuda.cu"], 
+            name = 'ops.tri_op_cuda', 
+            sources=["ops/src/tril_api.cpp",
+                    "ops/src/trillnear_devox_diff_R.cpp",
+                    "ops/src/trillnear_devox_diff_R_cuda.cu"], 
             include_dirs=[include_dirs]),
         ],
         cmdclass={
@@ -38,34 +38,3 @@ if __name__ == '__main__':
         }
     )
 
-
-# from setuptools import setup
-# import os
-# # import glob
-# from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension
-# # import subprocess
-
-# # def get_git_commit_number():
-# #     if not os.path.exists('.git'):
-# #         return '0000000'
-
-# #     cmd_out = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
-# #     git_commit_number = cmd_out.stdout.decode('utf-8')[:7]
-# #     return git_commit_number
-
-
-
-# if __name__ == '__main__':
-#     include_dirs = os.path.dirname(os.path.abspath(__file__))
-#     # version = '0.1.0+%s' % get_git_commit_number() 
-#     # source_cpu = glob.glob(os.path.join(include_dirs, 'gpu', '*.cpp', "*.cu"))
-#     setup(
-#         name='tril_devox_gpu',
-#         version="0.1",
-#         ext_modules=[
-#             CUDAExtension('tril_devox_gpu', sources=["src/trillnear_devox_diff_R.cpp", "src/trillnear_devox_diff_R_cuda.cu"], include_dirs=[include_dirs]),
-#         ],
-#         cmdclass={
-#             'build_ext': BuildExtension
-#         }
-#     )
